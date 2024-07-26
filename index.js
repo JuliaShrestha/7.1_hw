0d9c20a8361ec7fecaa209fa89b8e9561e68701b
JuliaShrestha <juliashrestha215@gmail.com>
2024-07-26 17:11:21 +0300

    Closures

--- index.js
+++ index.js
@@ -2,26 +2,24 @@
 Написати функцію, яка приймає 1 параметр. Та складає значення з тим, що передали перший раз і т. д. Все це із замиканнями, наприклад:
 
 console.log(sum(4)); // 4
-
 console.log(sum(6)); // 10
-
 console.log(sum(10)); // 20
-
 console.log(sum(7)); // 27
 */
-let sum = 0;
 
-function getSum (sum) {
-    
+function getSum () {
+
+   let sum = 0; 
+
     return function (number) {
         sum += number;
         return sum;
     }
 };
 
-const result = getSum(sum);
+const result = getSum();
 
 console.log(result (4));
 console.log(result (6));
-console.log(result(10));
-console.log(result(7));
+console.log(result (10));
+console.log(result (54));
