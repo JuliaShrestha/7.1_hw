aa274f6b1c43f750893d190ba9dfa09b5232f48d
JuliaShrestha <juliashrestha215@gmail.com>
2024-07-26 15:45:27 +0300

    Closures

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ console.log(sum(10)); // 20
 
 console.log(sum(7)); // 27
 */
+let sum = 0;
 
-function getSum () {
-    let sum = 0;
+function getSum (sum) {
     
     return function (number) {
         sum += number;
@@ -19,7 +19,7 @@ function getSum () {
     }
 };
 
-const result = getSum();
+const result = getSum(sum);
 
 console.log(result (4));
 console.log(result (6));
